bde68f5590c615237ede9178c98f8be5b67eafee
Zach-Richling <zwrdude@gmail.com>
2026-08-15 00:20:43 -0500

    feat: Add PipelineManager to manage and run pipelines concurrently

---
+++ Pipeline/PipelineManager.cs
@@ -0,0 +1,26 @@
+﻿namespace ChannelETL.Pipeline;
+
+public class PipelineManager
+{
+    private List<IPipeline> _pipelines = [];
+
+    public async Task RunAsync(CancellationToken token)
+    {
+        try
+        {
+            await Task.WhenAll(_pipelines.Select(x => x.RunAsync(token)));
+        }
+        catch (Exception)
+        {
+            //TODO: log here?
+        }
+    }
+
+    public void AddPipeline(IPipeline pipeline)
+    {
+        if (!_pipelines.Contains(pipeline))
+        {
+            _pipelines.Add(pipeline);
+        }
+    }
+}
