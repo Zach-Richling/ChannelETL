ba478c7df1b7088271658b90ac69e8cb6ab84473
Zach-Richling <zwrdude@gmail.com>
2026-08-19 14:17:22 -0500

    chore: update SuppressMessage for correct namespace

--- ChannelETL.Extensions.DependencyInjection/GlobalSuppressions.cs
+++ ChannelETL.Extensions.DependencyInjection/GlobalSuppressions.cs
@@ -1,3 +1,3 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 
-[assembly: SuppressMessage("Style", "IDE0130:Namespace does not match folder structure", Justification = "All API classes are in the ChannelETL namespace", Scope = "namespace", Target = "~N:ChannelETL")]
+[assembly: SuppressMessage("Style", "IDE0130:Namespace does not match folder structure", Justification = "All API classes are in the ChannelETL namespace", Scope = "namespace", Target = "~N:ChannelETL.Extensions.DependencyInjection")]
