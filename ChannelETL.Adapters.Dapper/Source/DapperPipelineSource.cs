eaf5d3952c30b470b318b5c662ac171bf3f9d85f
Zach-Richling <zwrdude@gmail.com>
2026-08-21 15:50:47 -0500

    feat: add a dapper pipeline source

---
+++ ChannelETL.Adapters.Dapper/Source/DapperPipelineSource.cs
@@ -0,0 +1,32 @@
+﻿using Dapper;
+using System.Data;
+using System.Data.Common;
+using System.Runtime.CompilerServices;
+
+namespace ChannelETL.Adapters.Dapper;
+
+public abstract class DapperPipelineSource<TSource>(DbConnection connection) : IPipelineSource<TSource>
+{
+    protected CommandType CommandType { get; init; } = CommandType.Text;
+    protected string? Text { get; init; }
+    protected string? TableName { get; init; }
+    protected string? StoredProcedureName { get; init; }
+    protected object? Parameters { get; init; }
+
+    private string Sql => CommandType switch
+    {
+        CommandType.Text => Text ?? throw new ArgumentException("Text must be provided for CommandType.Text.", nameof(Text)),
+        CommandType.StoredProcedure => StoredProcedureName ?? throw new ArgumentException("StoredProcedureName must be provided for CommandType.StoredProcedure.", nameof(StoredProcedureName)),
+        CommandType.TableDirect => TableName ?? throw new ArgumentException("TableName must be provided for CommandType.TableDirect.", nameof(TableName)),
+        _ => throw new NotSupportedException($"CommandType '{CommandType}' is not supported.")
+    };
+
+    public async IAsyncEnumerable<TSource> ProduceAsync([EnumeratorCancellation] CancellationToken token)
+    {
+        await foreach (var item in connection.QueryUnbufferedAsync<TSource>(Sql, Parameters, commandType: CommandType))
+        {
+            token.ThrowIfCancellationRequested();
+            yield return item;
+        }
+    }
+}
