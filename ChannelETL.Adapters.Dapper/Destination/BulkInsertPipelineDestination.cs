b429e2ee2052f08b86ff842d2647f98f2c95a0ef
Zach-Richling <zwrdude@gmail.com>
2026-08-21 18:08:56 -0500

    feat: add BulkInsertPipelineDestination to facilitate efficient bulk inserts of data
    - No reflection compiled expression of each type to convert to an object[], used to add all data in a batch to a datatable

---
+++ ChannelETL.Adapters.Dapper/Destination/BulkInsertPipelineDestination.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace ChannelETL.Adapters.Dapper;
+
+public abstract class BulkInsertPipelineDestination<TDestination>(SqlConnection connection, string tableName, int batchSize) : BatchedPipelineDestination<TDestination>(batchSize)
+{
+    public override async Task ConsumeBatchAsync(IReadOnlyList<TDestination> batch, CancellationToken token)
+    {
+        if (connection.State != ConnectionState.Open)
+        {
+            await connection.OpenAsync(token);
+        }
+
+        var data = batch.ToDataTable();
+
+        var options = SqlBulkCopyOptions.TableLock | SqlBulkCopyOptions.UseInternalTransaction;
+        using var bulkCopy = new SqlBulkCopy(connection, options, null)
+        {
+            DestinationTableName = tableName,
+            BulkCopyTimeout = 0
+        };
+
+        foreach (DataColumn column in data.Columns)
+        {
+            var colName = column.ColumnName;
+            bulkCopy.ColumnMappings.Add(colName, colName);
+        }
+
+        await bulkCopy.WriteToServerAsync(data, token);
+    }
+}
---
+++ ChannelETL.Adapters.Dapper/Destination/DataTableExtensions.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Concurrent;
+using System.Data;
+using System.Linq.Expressions;
+using System.Reflection;
+
+internal static class DataTableExtensions
+{
+    private static readonly ConcurrentDictionary<Type, object> _delegateCache = new();
+
+    public static DataTable ToDataTable<T>(this IEnumerable<T> data)
+    {
+        var table = new DataTable(typeof(T).Name);
+        var properties = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+        foreach (var prop in properties)
+        {
+            var propType = Nullable.GetUnderlyingType(prop.PropertyType) ?? prop.PropertyType;
+            table.Columns.Add(prop.Name, propType);
+        }
+
+        var extractor = (Func<T, object[]>)_delegateCache.GetOrAdd(typeof(T), _ => CompileExtractor<T>(properties));
+
+        table.BeginLoadData();
+        try
+        {
+            foreach (T item in data)
+            {
+                if (item != null)
+                {
+                    var extracted = extractor(item);
+                    table.Rows.Add(extracted);
+                }
+            }
+        }
+        finally
+        {
+            table.EndLoadData();
+        }
+
+        return table;
+    }
+
+    private static Func<T, object[]> CompileExtractor<T>(PropertyInfo[] properties)
+    {
+        var inputParam = Expression.Parameter(typeof(T), "item");
+        var elementExpressions = new Expression[properties.Length];
+
+        for (int i = 0; i < properties.Length; i++)
+        {
+            var propAccess = Expression.Property(inputParam, properties[i]);
+            var convert = Expression.Convert(propAccess, typeof(object));
+
+            elementExpressions[i] = Expression.Coalesce(
+                convert,
+                Expression.Constant(DBNull.Value, typeof(object))
+            );
+        }
+
+        var arrayExpression = Expression.NewArrayInit(typeof(object), elementExpressions);
+
+        return Expression.Lambda<Func<T, object[]>>(arrayExpression, inputParam).Compile();
+    }
+}
